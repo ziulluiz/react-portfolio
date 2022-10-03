bbea664503fabc2f124a1ec701410eaae9b58e8e
ziulluiz <luis.guzp13@gmail.com>
2022-10-03 17:37:56 -0500

    contact section done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-import './contact.css';
+import './contact.css'
+import { FiMail } from 'react-icons/fi'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
+
 
 export const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className='contact_option'>
+            <FiMail className='contact_option-icon'/>
+            <h4>Email</h4>
+            <h5>luis.guzp13@gmail.com</h5>
+            <a href="mailto:luis.guzp13@gmail.com" targer='_blank'>Send a Mail</a>
+          </article>
+          <article className='contact_option'>
+            <BsLinkedin className='contact_option-icon'/>
+            <h4>linkedIn</h4>
+            <h5>Luis Guzmán</h5>
+            <a href="https://www.linkedin.com/in/luis-guzp/" targer='_blank'>Go To Profile</a>
+          </article>
+          <article className='contact_option'>
+            <BsWhatsapp className='contact_option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+525538856480</h5>
+            <a href="https://api.whatsapp.com/send?phone=+525538856480" targer='_blank'>Send a Message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from "react-icons/bs"
+import { BsTwitter } from "react-icons/bs"
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>LUIS GP</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/luis-guzp/"><BsLinkedin/></a>
+        <a href="https://github.com/ziulluiz"><BsGithub/></a>
+        <a href=""></a>
+      </div>
+    </footer>
   )
 }
